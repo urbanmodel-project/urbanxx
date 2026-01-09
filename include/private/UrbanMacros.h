96357afe15b25dc444d0bc6f761385dc2661307c
Gautam Bisht <gautam.bisht@pnnl.gov>
2026-01-08 20:09:01 -0800

    Fixes trailing semicolons

--- include/private/UrbanMacros.h
+++ include/private/UrbanMacros.h
@@ -16,14 +16,14 @@ namespace URBANXX {
   viewname = type(#viewname, __VA_ARGS__);
 
 #define ALLOCATE_DEVICE_VIEW(viewname, type, ...)                              \
-  ALLOCATE_VIEW(viewname, type, __VA_ARGS__);
+  ALLOCATE_VIEW(viewname, type, __VA_ARGS__)
 #define ALLOCATE_HOST_VIEW(viewname, type, ...)                                \
-  ALLOCATE_VIEW(viewname##H, type, __VA_ARGS__);
+  ALLOCATE_VIEW(viewname##H, type, __VA_ARGS__)
 
 // Macro to allocate dual views (host and device)
 #define ALLOCATE_DUAL_VIEWS(viewname, suffix, ...)                             \
-  ALLOCATE_VIEW(viewname##H, HostArray##suffix, __VA_ARGS__);                  \
-  ALLOCATE_VIEW(viewname, Array##suffix, __VA_ARGS__);
+  ALLOCATE_VIEW(viewname##H, HostArray##suffix, __VA_ARGS__)                   \
+  ALLOCATE_VIEW(viewname, Array##suffix, __VA_ARGS__)
 
 } // namespace URBANXX
 
