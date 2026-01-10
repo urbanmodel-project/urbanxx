49dcef05c71bd415649f7fd68f71c18585a8f04d
Gautam Bisht <gautam.bisht@pnnl.gov>
2026-01-10 12:30:07 -0800

    Consolidate SetView functions

--- src/AtmosphereType.cpp
+++ src/AtmosphereType.cpp
@@ -1,6 +1,7 @@
 #include "Urban.h"
 #include "private/AtmosphereTypeImpl.h"
 #include "private/DataTypesImpl.h"
+#include "private/UrbanSetterHelpers.h"
 #include "private/UrbanTypeImpl.h"
 #include "private/UrbanValidation.h"
 
@@ -8,39 +9,6 @@
 struct _p_UrbanType : public URBANXX::_p_UrbanType {
   using URBANXX::_p_UrbanType::_p_UrbanType;
 };
-
-// Base template function for setting 1D host views
-template <typename ViewType>
-static void SetView1D(ViewType &view, const double *values, int length,
-                      UrbanErrorCode *status) {
-  using namespace URBANXX;
-
-  if (values == nullptr || status == nullptr) {
-    if (status)
-      *status = URBAN_ERR_INVALID_ARGUMENT;
-    return;
-  }
-
-  try {
-    // Check if the length matches the view extent
-    if (length != static_cast<int>(view.extent(0))) {
-      *status = URBAN_ERR_SIZE_MISMATCH;
-      return;
-    }
-
-    // Create an unmanaged host view from the input array
-    auto values_view =
-        Kokkos::View<const double *, Kokkos::HostSpace,
-                     Kokkos::MemoryTraits<Kokkos::Unmanaged>>(values, length);
-
-    // Deep copy from the temporary host view to the host view
-    Kokkos::deep_copy(view, values_view);
-
-    *status = URBAN_SUCCESS;
-  } catch (...) {
-    *status = URBAN_ERR_INTERNAL;
-  }
-}
 
 extern "C" {
 
