3cf739c7a016935070e2f8c4f334a9d93ec951e6
Gautam Bisht <gautam.bisht@pnnl.gov>
2026-02-04 20:13:55 -0800

    Moves functions from header to cpp

--- src/UrbanHeatDiffusion.cpp
+++ src/UrbanHeatDiffusion.cpp
@@ -31,6 +31,96 @@ Real ComputeGroundNetEnergyFluxDerivative(Real cgrnds, Real cgrndl, Real emiss,
   return -cgrnd - dlwrdDTemp;
 }
 
+// Compute effective thermal conductivity for pervious road soil layers
+// Based on ELM SoilTemperatureMod.F90 (Johansen 1975 model)
+template <typename ViewType>
+KOKKOS_INLINE_FUNCTION void ComputeSoilThermalConductivity(
+    const int l, const int numLayers, const ViewType &tk_minerals,
+    const ViewType &tk_dry, const ViewType &tkLayer, const ViewType &watsat,
+    const ViewType &water_liquid, const ViewType &water_ice, const ViewType &dz,
+    const ViewType &temp) {
+
+  constexpr Real tfrz = SHR_CONST_TKFRZ;
+  constexpr Real denh2o = SHR_CONST_RHOWATER;
+  constexpr Real denice = SHR_CONST_RHOICE;
+  constexpr Real tkwat = TKWATER;
+  constexpr Real tkice = TKICE;
+
+  for (int k = 0; k < numLayers; ++k) {
+    // Compute degree of saturation
+    // satw = (liquid_vol + ice_vol) / porosity
+    const Real satw_raw =
+        (water_liquid(l, k) / denh2o + water_ice(l, k) / denice) /
+        (dz(l, k) * watsat(l, k));
+    const Real satw = Kokkos::fmin(1.0, satw_raw);
+
+    if (satw > 1.0e-6) {
+      // Compute Kersten number (thermal conductivity enhancement factor)
+      Real dke;
+      if (temp(l, k) >= tfrz) {
+        // Unfrozen soil: Kersten number based on log of saturation
+        dke = Kokkos::fmax(0.0, Kokkos::log10(satw) + 1.0);
+      } else {
+        // Frozen soil: Kersten number equals saturation
+        dke = satw;
+      }
+
+      // Compute liquid fraction
+      const Real liq_vol = water_liquid(l, k) / (denh2o * dz(l, k));
+      const Real ice_vol = water_ice(l, k) / (denice * dz(l, k));
+      const Real fl = liq_vol / (liq_vol + ice_vol);
+
+      // Saturated thermal conductivity (Johansen model)
+      // Geometric mean of components weighted by porosity and phase
+      const Real dksat = tk_minerals(l, k) *
+                         Kokkos::pow(tkwat, fl * watsat(l, k)) *
+                         Kokkos::pow(tkice, (1.0 - fl) * watsat(l, k));
+
+      // Effective thermal conductivity
+      // Linear interpolation between dry and saturated based on Kersten number
+      tkLayer(l, k) = dke * dksat + (1.0 - dke) * tk_dry(l, k);
+    } else {
+      // Dry soil
+      tkLayer(l, k) = tk_dry(l, k);
+    }
+    if (k > NUM_LAYERS_ABV_BEDROCK - 1) {
+      tkLayer(l, k) = TK_BEDROCK;
+    }
+  }
+}
+
+// Compute heat capacity times layer thickness for pervious road soil layers
+// Based on ELM SoilTemperatureMod.F90
+// cv(c,j) = csol(c,j)*(1-watsat(c,j))*dz(c,j) + (h2osoi_ice(c,j)*cpice +
+// h2osoi_liq(c,j)*cpliq)
+template <typename ViewType>
+KOKKOS_INLINE_FUNCTION void ComputeSoilHeatCapacityTimesDz(
+    const int l, const int numLayers, const ViewType &cv_solids,
+    const ViewType &watsat, const ViewType &water_liquid,
+    const ViewType &water_ice, const ViewType &dz, const ViewType &cvTimesDz) {
+
+  constexpr Real cpice = SHR_CONST_CPICE; // Specific heat of ice (J/kg/K)
+  constexpr Real cpliq =
+      SHR_CONST_CPFW; // Specific heat of liquid water (J/kg/K)
+
+  for (int k = 0; k < numLayers; ++k) {
+    // Heat capacity of soil solids component
+    const Real cv_solid_component =
+        cv_solids(l, k) * (1.0 - watsat(l, k)) * dz(l, k);
+
+    // Heat capacity of water components (ice + liquid)
+    const Real cv_water_component =
+        water_ice(l, k) * cpice + water_liquid(l, k) * cpliq;
+
+    // Total heat capacity times layer thickness
+    if (k < 10) {
+      cvTimesDz(l, k) = cv_solid_component + cv_water_component;
+    } else {
+      cvTimesDz(l, k) = cv_solid_component;
+    }
+  }
+}
+
 // Solve tridiagonal system using Thomas algorithm
 // Solves: a[i]*x[i-1] + b[i]*x[i] + c[i]*x[i+1] = r[i]
 // for i = 0 to n-1
